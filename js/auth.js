a5d3ced484096c664a6c0a14edcfd224419749cb
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-22 12:46:02 +0000

    Add user authentication functions in auth.js

---
+++ js/auth.js
@@ -0,0 +1,166 @@
+alert("auth.js connected");
+
+
+// ================= REGISTER USER =================
+
+function registerUser(){
+
+let name =
+document.getElementById("registerName").value.trim();
+
+let email =
+document.getElementById("registerEmail").value.trim();
+
+let password =
+document.getElementById("registerPassword").value;
+
+
+if(!name || !email || !password){
+
+alert("Please complete all fields");
+return;
+
+}
+
+
+let users =
+JSON.parse(localStorage.getItem("users")) || [];
+
+
+
+let exists =
+users.find(function(user){
+
+return user.email === email;
+
+});
+
+
+
+if(exists){
+
+alert("Account already exists");
+return;
+
+}
+
+
+
+let user = {
+
+id: Date.now(),
+
+name:name,
+
+email:email,
+
+password:password
+
+};
+
+
+
+users.push(user);
+
+
+
+localStorage.setItem(
+"users",
+JSON.stringify(users)
+);
+
+
+
+alert("Registration successful");
+
+
+window.location.href="login.html";
+
+
+}
+
+
+
+// ================= LOGIN USER =================
+
+
+function loginUser(){
+
+
+let email =
+document.getElementById("loginEmail").value.trim();
+
+
+
+let password =
+document.getElementById("loginPassword").value;
+
+
+
+let users =
+JSON.parse(localStorage.getItem("users")) || [];
+
+
+
+let user =
+users.find(function(user){
+
+return user.email === email &&
+user.password === password;
+
+});
+
+
+
+if(!user){
+
+alert("Wrong email or password");
+return;
+
+}
+
+
+
+localStorage.setItem(
+"user",
+JSON.stringify(user)
+);
+
+
+
+localStorage.setItem(
+"loggedIn",
+"true"
+);
+
+
+
+alert(
+"Welcome " + user.name
+);
+
+
+
+window.location.href="../index.html";
+
+
+}
+
+
+
+// ================= LOGOUT =================
+
+
+function logoutUser(){
+
+localStorage.removeItem("user");
+
+localStorage.removeItem("loggedIn");
+
+
+alert("Logged out");
+
+
+window.location.href="login.html";
+
+}
