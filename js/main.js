2439038323c9fae60eaeefbd0e95ecb2189fa572
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-22 13:13:56 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,63 @@
-alert("Product Finder v2 connected");
+alert("main.js connected");
+
+
+// ================= PAGE LOADER =================
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    console.log("Product Finder loaded");
+
+    updateCartCount();
+
+});
+
+
+
+// ================= CART COUNT =================
+
+function updateCartCount(){
+
+    let cart =
+    JSON.parse(localStorage.getItem("cart")) || [];
+
+
+    let count = 0;
+
+
+    cart.forEach(function(item){
+
+        count += Number(item.quantity || 1);
+
+    });
+
+
+
+    let cartBox =
+    document.getElementById("cartCount");
+
+
+    if(cartBox){
+
+        cartBox.innerText = count;
+
+    }
+
+}
+
+
+
+// ================= LOGOUT =================
+
+function logout(){
+
+    localStorage.removeItem("user");
+
+    localStorage.removeItem("merchant");
+
+
+    alert("Logged out");
+
+
+    window.location.href="index.html";
+
+}
