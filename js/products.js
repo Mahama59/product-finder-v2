8d64503158376eac1522664964cc4036a2e2a2c8
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-28 11:10:54 +0000

    Allow pending products in approved products filter

--- js/products.js
+++ js/products.js
@@ -155,7 +155,7 @@ JSON.parse(localStorage.getItem("merchantProducts")) || [];
 let approvedProducts =
 products.filter(function(product){
 
-return product.status === "Approved";
+return product.status === "Approved" || product.status === "Pending";
 
 });
 
