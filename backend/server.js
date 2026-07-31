5534ed9cec44dbd8e673b1e6765695cae11a6fbc
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-31 14:07:32 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const crypto = require("crypto");
-
+const prisma =
+    require("./prismaClient");
 const app = express();
 
 const PORT =
